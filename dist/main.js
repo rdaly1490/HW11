4677d332df5328ce1c859cab9559cdd3a3a89ef4
Rob <rdaly1490@gmail.com>
2015-05-21 13:07:48 -0500

    just need to figure out when to clear section

--- dist/main.js
+++ dist/main.js
@@ -5,7 +5,17 @@ var messageList = document.getElementById("message-list");
 
 myButton.addEventListener("click", onButtonClick);
 
+
 function onButtonClick(eventObj) {
 	console.log(messageInput.value);
+	list.push(messageInput.value);
+	console.log(list);
+	messageInput.value = "";
 }
 
+function render(eventObj) {
+	messageList.value = "";
+	var listToString = "<div>" + list[i].join("</div><div>") + "</div>";
+	console.log(listToString);
+	messageList.value = listToString;
+}
